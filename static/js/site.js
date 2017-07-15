4ca2b75391f1b5877d97f5f4cd1277d523406661
merrick.heley <merrick.heley@gmail.com>
2017-07-15 21:56:34 +1000

    Write the algorithm that makes the magic happen

--- static/js/site.js
+++ static/js/site.js
@@ -9,27 +9,82 @@ var db = {
 		"lon": 153.038282,
 		"lat": -27.452363,
 	},
-	"startTime": 0700,
-	"finalTime": 0900,
+	"startTime": {
+		"hour": 7,
+		"min": 0,
+	},
+	"finalTime": {
+		"hour": 9,
+		"min": 0,
+	},
 };
 
-function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
-  directionsService.route({
-	origin: pointA,
-	destination: pointB,
-	travelMode: google.maps.TravelMode.TRANSIT
-  }, function(response, status) {
-	if (status == google.maps.DirectionsStatus.OK) {
-	  directionsDisplay.setDirections(response);
-	} else {
-	  window.alert('Directions request failed due to ' + status);
+minimum = undefined;
+minute=60*1000;
+function minimiseRoute(startPos, finalPos, startTime, finalTime, finalCallback) {
+		
+	if (startTime.getTime() < finalTime.getTime()) {
+		directionsService.route({
+			origin: startPos,
+			destination: finalPos,
+			travelMode: google.maps.TravelMode.TRANSIT,
+			transitOptions: {
+				arrivalTime: startTime,
+			}
+		}, function(dirResult, dirStatus) {
+			if (dirStatus == google.maps.DirectionsStatus.OK) {
+				console.log(startTime.getHours(), startTime.getMinutes(),
+					dirResult.routes[0].legs[0].duration.text);
+				
+				if ((minimum === undefined) || (dirResult.routes[0].legs[0].duration.value < minimum.routes[0].legs[0].duration.value)) {
+					minimum = dirResult;
+				}
+			} else {
+				console.log(startTime.getHours(), startTime.getMinutes(),
+					'Directions request failed due to ' + dirStatus
+				);
+			}
+						
+			window.setTimeout(function () {
+					minimiseRoute(startPos, finalPos, 
+						new Date(startTime.getTime() + 10*minute), 
+						finalTime, finalCallback
+					);
+			}, 200);
+		});
 	}
-  });
+	else {
+		// call final callback
+		finalCallback(minimum);
+	}
+	
 }
 
 function initMap() {
+	
+	gStartPos = new google.maps.LatLng(db.startPos.lat, db.startPos.lon);
+	gFinalPos = new google.maps.LatLng(db.finalPos.lat, db.finalPos.lon);
+	
+	now = new Date();
+	
+	if (now.getHours() > db.startTime.hour) {
+		tomorrow = 1;
+	} else {
+		tomorrow = 0;
+	}
+	
+	gStartTime = new Date(
+		now.getFullYear(), now.getMonth(), now.getDate()+tomorrow,
+		db.startTime.hour, db.startTime.min, 0, 0
+	);
+		
+	gFinalTime = new Date(
+		now.getFullYear(), now.getMonth(), now.getDate()+tomorrow,
+		db.finalTime.hour, db.finalTime.min, 0, 0
+	);
+	
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: db.startPos.lat, lng: db.startPos.lon},
+		center: gStartPos,
 		zoom: 14
 	});
 	// Instantiate a directions service.
@@ -40,11 +95,18 @@ function initMap() {
 			suppressMarkers: true,
 		}
 	});
+	
+	minimiseRoute(gStartPos, gFinalPos, gStartTime, gFinalTime, function (route) {
+		if (!!route) {
+			directionsDisplay.setDirections(route);
+			
+			// route.routes[0].legs[0].arrival_time
+			// route.routes[0].legs[0].departure_time
+			// route.routes[0].legs[0].duration.text
+		}
+	});
 
-	calculateAndDisplayRoute(directionsService, directionsDisplay,
-		new google.maps.LatLng(db.startPos.lat, db.startPos.lon),
-		new google.maps.LatLng(db.finalPos.lat, db.finalPos.lon)
-	);
+
 }
 $(document).ready(function() {
 	
